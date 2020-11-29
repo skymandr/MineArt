10ef08f062ba7fae6c597343458845389f2ef7c6
Andreas Skyman <andreas.skyman@molflow.com>
2020-11-29 20:04:25 +0100

    add gpl-header

--- minart.py
+++ minart.py
@@ -1,4 +1,20 @@
 #! /usr/bin/env python3
+"""mineart.py: Simple program for translating images to MineCraft blocks
+    Copyright (C) 2020 Andreas Skyman
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
 from argparse import ArgumentParser
 from typing import Optional, Tuple
 
